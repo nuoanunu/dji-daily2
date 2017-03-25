d725f5d4af3c8ea2be5a08fe8624ec35895a4b9b
nuoanunu <dwarpro@gmail.com>
2017-03-25 10:24:26 +0700

    a
    a

---
+++ djiDaiLy/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using djiDaiLy.Models;
+namespace djiDaiLy.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult SearchWarranty()
+        {
+            ThienNgaDatabaseEntities entityManager = new ThienNgaDatabaseEntities();
+            ViewData["warranties"] = entityManager.tb_warranty.Where(u => u.MaChinh != null && u.MaChinh == true).ToList();
+            return View("SearchWarranty");
+        }
+        public ActionResult WarrantyDetail(int id)
+        {
+            ThienNgaDatabaseEntities entityManager = new ThienNgaDatabaseEntities();
+            ViewData["warranty"] = entityManager.tb_warranty.Find(id);
+            return View("ViewWarrantyDetail");
+        }
+    }
+}
